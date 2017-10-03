0a987592bf6513b4483a66cb9f1724ab07f9f118
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 16:25:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(array) {
 
 function currentLine(array) {
   for (var i = 0; i < array.length; i++) {
-    if (i > 0) {
+    if (array.length > 0) {
       return "The line is currently: " + i + 1 + ". " + array[i] + ", "
     } else {
       return "The line is currently empty.";
