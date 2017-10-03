e004f0650794ec1dfdf9e73bfc41b197c1dc8fa8
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 16:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeli.push(name);
-  console.log("Welcome, " + name + ". You are number " + katzDeli[name + 1] + " in line.")
+  console.log("Welcome, " + name + ". You are number " + katzDeli[name] + " in line.")
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeli.length == 0; ) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving katzDeli[0]"; 
+    katzDeli.shift();
+  }
+  
+  
 }
