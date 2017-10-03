d843c0a6a7913a100fad87d384dcb4656a3e0076
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 16:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(array) {
     var string = "The line is currently: ";
     if (array.length > 0) {
       string = string + (i + 1) + ". " + array[i] + ", "
-    } else {
+    } else if (array.length == 0) {
       return "The line is currently empty.";
     }
 
