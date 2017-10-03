c7d0a606008ef9c749844ffb265ce511aedf5e85
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 16:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function nowServing(array) {
   if (array.length == 0) {
     return "There is nobody waiting to be served!"
   } else {
+    var first = array[0];
     array.shift();
-    return "Currently serving " + array[0] + ".";
+    return "Currently serving " + first + ".";
 
   }
 }
