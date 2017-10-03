0f7e287a5a825c4f4a2689c75e651d6226af811c
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 16:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeli.push(name);
-  console.log("Welcome, " + name + "You are number " + "_" + " in line.")
+  console.log("Welcome, " + name + ". You are number " + katzDeli[name + 1] + " in line.")
 }
